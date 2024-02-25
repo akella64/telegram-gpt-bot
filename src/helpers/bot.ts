3c0c315614c181970c02e2cafbef03f4eaa5f846
Александр Алимов <sanek1998_ax@mail.ru>
2024-02-25 13:52:33 +0300

    structure bot.ts

--- src/helpers/bot.ts
+++ src/helpers/bot.ts
@@ -1,6 +1,50 @@
-import { Bot } from 'grammy';
+import { Bot, Keyboard, InlineKeyboard } from 'grammy';
 
-const token = process.env.BOT_TOKEN;
-if (!token) throw new Error('Телеграм бот токен отсутствует');
+class TelegramBot {
+	public bot;
 
-export const bot = new Bot(token);
+	constructor() {
+		this.bot = this.init();
+	}
+
+	private init() {
+		const token = process.env.BOT_TOKEN;
+		if (!token) throw new Error('Телеграм бот токен отсутствует');
+		const bot = new Bot(token);
+
+		return bot;
+	}
+
+	public keyboardMenu(actualModel: string) {
+		const keyboard = new Keyboard()
+			.text('Очистить историю диалога')
+			.row()
+			.text('Мои чаты')
+			.row()
+			.text(`Изменить версию GPT (Текущая: ${actualModel})`)
+			.row()
+			.resized();
+
+		return keyboard;
+	}
+
+	public inlineModels() {
+		const models = new InlineKeyboard()
+			.text('gpt-3.5-turbo', 'gpt-3.5-turbo')
+			.row()
+			.text('gemini-pro', 'gemini-pro')
+			.row()
+			.text('gemini-pro-vision', 'gemini-pro-vision')
+			.row()
+			.text('llama-2-70b-chat', 'llama-2-70b-chat')
+			.row()
+			.text('mistral-7b', 'mistral-7b')
+			.row()
+			.text('mixtral-8x7', 'mixtral-8x7')
+			.row();
+
+		return models;
+	}
+}
+
+export default TelegramBot;
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,26 @@
-import { bot } from './helpers/bot';
+import TelegramBot from './helpers/bot';
 import GptOpenAi from './helpers/openai';
-import { Keyboard } from 'grammy';
 
 // gpt init
 const gptOpenAi = new GptOpenAi();
 
-// Menu
-const keyboard = new Keyboard()
-	.text('Очистить историю диалога')
-	.row()
-	.text('Мои чаты')
-	.row()
-	.text(`Изменить версию GPT (Текущая: ${gptOpenAi.model})`)
-	.row()
-	.resized();
+// Bot init
+const telegramBot = new TelegramBot();
 
 // Bot Start command
-bot.command('start', ctx => {
+telegramBot.bot.command('start', ctx => {
 	const name: string = ctx.message?.from?.first_name || '';
 
 	return ctx.reply(
 		`Привет ${name}! Я GPT Bot - твой персональный собеседник и помощник в чате. Я готов обсудить с тобой что угодно: от последних новостей до творчества и науки. Просто напиши мне, и давай начнем наше общение!`,
 		{
-			reply_markup: keyboard,
+			reply_markup: telegramBot.keyboardMenu(gptOpenAi.model),
 		},
 	);
 });
 
 // Help
-bot.command('help', ctx =>
+telegramBot.bot.command('help', ctx =>
 	ctx.reply(`
 🏁 Для начала диалога с ботом, начните писать в чат.\n
 🧹 Для очистки текущего диалога нажмите кнопку "Очистить историю диалога" в меню.\n
@@ -37,68 +29,46 @@ bot.command('help', ctx =>
 );
 
 // Clear history dialog
-bot.hears('Очистить историю диалога', ctx => {
+telegramBot.bot.hears('Очистить историю диалога', ctx => {
 	gptOpenAi.clearChat();
 	ctx.reply('История текущего чата очищена.');
 });
 
-bot.hears('Мои чаты', ctx => {
+// View chats
+telegramBot.bot.hears('Мои чаты', ctx => {
 	ctx.reply('В разработке');
 });
 
-// Change version GPT
-/* const versionsGpt = new InlineKeyboard()
-	.text('gpt-3.5-turbo', 'gpt-3.5-turbo')
-	.row()
-	.text('gemini-pro', 'gemini-pro')
-	.row()
-	.text('gemini-pro-vision', 'gemini-pro-vision')
-	.row()
-	.text('llama-2-70b-chat', 'llama-2-70b-chat')
-	.row()
-	.text('mistral-7b', 'mistral-7b')
-	.row()
-	.text('mixtral-8x7', 'mixtral-8x7')
-	.row();
-
-versionsGpt.inline_keyboard.forEach(row => {
+// Change version GPT (in development)
+/* telegramBot.inlineModels().inline_keyboard.forEach(row => {
 	row.forEach(button => {
-		bot.callbackQuery(button.text, async ctx => {
-			const keyboard = new Keyboard()
-				.text('Очистить историю диалога')
-				.row()
-				.text('Мои чаты')
-				.row()
-				.text(`Изменить версию GPT (Текущая: ${button.text})`)
-				.row()
-				.resized();
-
-			//	РАЗОБРАТЬСЯ СО СМЕНОЙ МОДЕЛИ
-
+		telegramBot.bot.callbackQuery(button.text, async ctx => {
 			gptOpenAi.clearChat();
 			gptOpenAi.setModel(button.text);
 
 			await ctx.deleteMessage();
 
 			await ctx.reply(`Версия GPT изменена на ${button.text}.`, {
-				reply_markup: keyboard,
+				reply_markup: telegramBot.keyboardMenu(button.text),
 			});
 		});
 	});
 }); */
 
-bot.hears(`Изменить версию GPT (Текущая: ${gptOpenAi.model})`, ctx => {
-	ctx.reply(
-		`Смена версии GPT находится в разработке. Пока что вы можете насладиться диалогом с ${gptOpenAi.model}.`,
-	);
-
-	/* 	ctx.reply('Изменить версию gpt', {
+telegramBot.bot.hears(
+	`Изменить версию GPT (Текущая: ${gptOpenAi.model})`,
+	ctx => {
+		ctx.reply(
+			`Смена версии GPT находится в разработке. Пока что вы можете насладиться диалогом с ${gptOpenAi.model}.`,
+		);
+		/* 	ctx.reply('Изменить версию gpt', {
 		reply_markup: versionsGpt,
 	}); */
-});
+	},
+);
 
 // User send message
-bot.on('message', async ctx => {
+telegramBot.bot.on('message', async ctx => {
 	const message: string = ctx.update.message?.text || '';
 
 	await ctx.api.sendChatAction(ctx.chat.id, 'typing');
@@ -106,4 +76,4 @@ bot.on('message', async ctx => {
 	await ctx.reply(await gptOpenAi.gptChat(message));
 });
 
-bot.start();
+telegramBot.bot.start();
